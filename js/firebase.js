c7075c50b0c146d5757eb4171e8fe1c0fd175b81
Gustavo Enrique Tovar Ramos <lintavo@protonmail.com>
2022-06-12 12:32:07 -0600

    Credenciales de firebase establecidas en archivo firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAov-ZdTrHFdWT-l0uN7VwiOQMo_UmXjvc",
+  authDomain: "gasesutec.firebaseapp.com",
+  databaseURL: "https://gasesutec-default-rtdb.firebaseio.com",
+  projectId: "gasesutec",
+  storageBucket: "gasesutec.appspot.com",
+  messagingSenderId: "870090363268",
+  appId: "1:870090363268:web:af9f3749d567261ac80dd1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
