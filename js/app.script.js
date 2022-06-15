c2191eedd88003734b9f1d1b7ade812933f6dbbf
Gustavo Enrique Tovar <lintavo@protonmail.com>
2022-06-15 15:55:00 -0600

    Sprint 4 (#30)
    * Archivo firebase.js creado dentro del directorio js del proyecto
    
    * Credenciales de firebase establecidas en archivo firebase.js
    
    * Archivo .jshintrc incluido en la raíz del proyecto para establecer que versión de JavaScript se ejecuta en el proyecto
    
    * Archivo firebase.js vinculado al index.html mediante etiqueta <script>
    
    * Gráfica de lineas agregada a la sección de análisis de la aplicación
    
    * Se removieron las clases que colapsaban las diferentes secciones de la aplicación y se actualizaron los vínculos del menú
    
    * Se eliminaron las funciones del menú anterior y se creo la función que crea la gráfica de PLG del último registro
    
    * Grafico del último registro de CO agregado al proyecto
    
    * Se agregó fecha y hora del ultimo registro a las tarjetas de monitoreo
    
    * Tabla agregada a la funcionalidad de la aplicación
    
    * Íconos de cabecera se agregaron a la tabla, tambien se habilitó el idioma español
    
    * Estilos aplicados a los botones de exportación e impresión de los datos de la tabla
    
    * Seremovió la opción de exportar data de tabla a pdf y se sustituyó por la opción de descargar la data en formato de archivo JSON
    
    * Se agregó año de forma dinámica en el footer de la aplicación
    
    * Se agregó la librería moment para el formateo de fechas en la aplicación
    
    * Seagregó la referencia al la librería moment en archivo index.html
    
    * Se aplico filtro en los datos mostrados por la gráfica de líneas y ahora solo muestra los registros correspondientes al dia en curso
    
    * Se habilitaron elementos popover de ayuda
    
    * Descripción de las herramientas de la aplicación actualizada
    
    * Ids para tarjeta de alerta creados
    
    * Responsive de aplicación a un 95%
    
    * Funcionalidad de la tarjeta de alertas completa
    
    * Archivos de imagen basura eliminados y se agregó la imagen que se utilizará en las alertas
    
    * Sección 'acerca de' de la aplicación terminada
    
    * Cambios efectuados a la barra de menú

--- js/app.script.js
+++ js/app.script.js
@@ -1,55 +1,310 @@
 /* Variables y constantes gobales */
+var myFirebaseRef;
+/* Gráfico de líneas */
+var chart;
+var chartPLG;
+var chartCO;
+var chartLinesData1 = ['PLG'];
+var chartLinesData2 = ['CO'];
+var chartLinesX = [];
+var chartLines = [];
+var chartLinesCat = [];
+var table = [];
+var dataset;
+var lecterPLG = ['PLG'];
+var lecterCO = ['CO'];
+var CO;
+var PLG;
+var date;
+var dateapp;
+var byetable;
 
 /* Recorrido de la página */
 $(document).ready(function() {
-    $('#inicio').click(viewHome);
-    $('#monitoreo').click(viewMonitoreo);
-    $('#analisis').click(viewAnalisis);
-    $('#datos').click(viewDatos);
-    $('#contacto').click(viewContact);
+    myFirebaseRef = new Firebase("https://gasesutec-default-rtdb.firebaseio.com/");
+    
+    requestData();
+    addChartLine();
+    addChartPLG();
+    addChartCO();
+    date = new Date();
+    $('#año').html(date.getFullYear());
+    //puuu(15,22);
 });
 
-/* Función botón inicio */
-viewHome = function() {
-    $('#monitoreo-tool-section').removeClass('show');
-    $('#analytics').removeClass('show');
-    $('#data').removeClass('show');
-    $('#contacts').removeClass('show');
-    $('#home-section').addClass('show');
+/* Función requestData */
+requestData = function(){
+
+    var IoT;
+    var any;
+
+    //$("#spinner").addClass('visible');
+
+    myFirebaseRef.on("value", function(data) {
+        var lecter = data.val();
+        //var arr;
+        var ultimate;
+        chartLines=[];
+        chartLinesData1 = ['PLG'];
+        chartLinesData2 = ['CO'];
+        chartLinesX = [];
+        lecterPLG = [];
+        lecterCO = [];
+
+        for(ultimate in lecter){
+            CO = (lecter[ultimate].ultimoDato.CO);
+            PLG = (lecter[ultimate].ultimoDato.PLG);
+        }
+        lecterPLG.push(['PLG', Number(PLG)]);
+        lecterCO.push(['CO', Number(CO)]);
+
+        var co = Number(CO);
+        var plg = Number(PLG);
+
+        $('#plg-ultimate').html(lecter[ultimate].ultimoDato.fecha + ' ' + lecter[ultimate].ultimoDato.hora);
+        $('#co-ultimate').html(lecter[ultimate].ultimoDato.fecha + ' ' + lecter[ultimate].ultimoDato.hora);
+
+        chartPLG.load({
+            columns: lecterPLG
+        });
+
+        chartCO.load({
+            columns: lecterCO
+        });
+
+        var arr1;
+        var arr2;
+        var arr3;
+        var regt;
+
+        for(IoT in lecter){
+            var history = lecter[IoT].historico;
+
+            for(any in history){
+                dateapp = moment().format('DD/MMMM/YYYY');
+                if (dateapp == history[any].fecha){
+                    arr1 = Number(history[any].PLG);
+                    chartLinesData1.push(arr1);
+    
+                    arr2 = Number(history[any].CO);
+                    chartLinesData2.push(arr2);
+    
+                    arr3 = history[any].fecha + ' ' + history[any].hora;
+                    chartLinesX.push(arr3);
+     
+                }
+                regt = [history[any].PLG, history[any].CO, history[any].fecha, history[any].hora];
+                table.push(regt);
+            }
+            llenarTabla();
+            chartLines.push(chartLinesData1);
+            chartLines.push(chartLinesData2);     
+            //chartLinesX.push(chartLinesCat);     
+            chart.load({
+                columns: chartLines,
+                categories: chartLinesX
+            });
+            puuu(plg, co);
+        }
+        
+        
+        //$("#total span").html(total);
+
+        //chart.load({
+        //    columns: chartLinesData
+        //});//Carga el gráfico con los datos obtenidos en el arreglo
+        //$("#spinner").addClass('invisible');
+    });
 };
 
-/* Función botón monitoreo */
-viewMonitoreo = function() {
-    $('#home-section').removeClass('show');
-    $('#analytics').removeClass('show');
-    $('#contacts').removeClass('show');
-    $('#data').removeClass('show');
-    $('#monitoreo-tool-section').addClass('show');
+/* Función para generar gráfica de líneas */
+addChartLine = function() {
+        chart = c3.generate({
+        bindto: '#graph-lines',
+        data: {
+            columns: chartLines
+        },
+        axis: {
+            x: {
+                type: 'category',
+                categories: chartLinesX,
+                show: false,
+                //label: 'Fecha y hora de la lectura'
+            }
+        }
+    });
+    //console.log(chartLinesData1);
 };
 
-/* Función botón analisis */
-viewAnalisis = function() {
-    $('#home-section').removeClass('show');
-    $('#monitoreo-tool-section').removeClass('show');
-    $('#data').removeClass('show');
-    $('#contacts').removeClass('show');
-    $('#analytics').addClass('show');
+/* Función para gráfica de monitoreo PGL */
+addChartPLG = function() {
+      chartPLG = c3.generate({
+        bindto: '#graph-plg',
+        data: {
+            columns: [ lecterPLG ],
+            type: 'gauge'
+        },
+        gauge: {
+            show: true,
+            min: 0,
+            max: 1300,
+            units: 'PPM'
+        },
+        color: {
+            pattern: ['#47704c', '#f59e01', '#e61348'],
+            threshold: {
+                values: [700, 1000, 1300]
+            }
+        }
+      });
+      console.log(lecterPLG);
 };
 
-/* Función botón datos */
-viewDatos = function() {
-    $('#home-section').removeClass('show');
-    $('#monitoreo-tool-section').removeClass('show');
-    $('#analytics').removeClass('show');
-    $('#contacts').removeClass('show');
-    $('#data').addClass('show');
+/* Función para gráfica de monitoreo PGL */
+addChartCO = function() {
+    chartCO = c3.generate({
+      bindto: '#graph-co',
+      data: {
+          columns: [ lecterCO ],
+          type: 'gauge'
+      },
+      gauge: {
+          show: true,
+          min: 0,
+          max: 130,
+          units: 'PPM'
+      },
+      color: {
+          pattern: ['#47704c', '#f59e01', '#e61348'],
+          threshold: {
+              values: [70, 100, 130]
+          }
+      }
+    });
+    console.log(lecterCO);
 };
 
-/* Función botón contacto */
-viewContact = function() {
-    $('#home-section').removeClass('show');
-    $('#monitoreo-tool-section').removeClass('show');
-    $('#analytics').removeClass('show');
-    $('#data').removeClass('show');
-    $('#contacts').addClass('show');
+/* LLenar tabla con datos */
+llenarTabla = function() {
+    $('#historico').DataTable({
+        data: table,
+        columns: [
+            { title: '<i class="bi bi-speedometer"></i> PLG' },
+            { title: '<i class="bi bi-speedometer"></i> CO' },
+            { title: '<i class="bi bi-calendar-date"></i> Fecha' },
+            { title: '<i class="bi bi-clock"></i> Hora' },
+        ],
+        retrieve: true,
+        paging: true,
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json'
+        },
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'copyHtml5',
+                text: '<i class="bi bi-clipboard2-plus"></i> Copiar',
+                titleAttr: 'Copiar datos',
+                footer: true
+            },
+            {
+                extend: 'csvHtml5',
+                text: '<i class="bi bi-filetype-csv"></i> CSV',
+                titleAttr: 'Descargar archivo csv',
+                footer: true
+            },
+            {
+                extend: 'excelHtml5',
+                text: '<i class="bi bi-file-spreadsheet"></i> Excel',
+                titleAttr: 'Descargar hoja de excel',
+                footer: true
+            },
+            /* {
+                extend: 'pdfHtml5',
+                text: '<i class="bi bi-filetype-pdf"></i> PDF',
+                titleAttr: 'Descargar PDF',
+                footer: true
+            }, */
+            {
+                text: '<i class="bi bi-filetype-json"></i> JSON',
+                action: function ( e, dt, button, config ) {
+					var data = dt.buttons.exportData();
+					$.fn.dataTable.fileSave(
+						new Blob( [ JSON.stringify( data ) ] ),
+						'Retavid - Sistema de monitoreo de gas propano.json'
+					);
+				},
+                titleAttr: 'Descargar archivo JSON',
+                footer: true
+            },
+            {
+                extend: 'print',
+                text: '<i class="bi bi-printer"></i> Imprimir',
+                titleAttr: 'Imprimir datos de la tabla',
+                footer: true,
+                class: 'btn-sm'
+            },
+        ],
+        responsive: true
+      });
+};
+
+/* Activar o no la tarjeta de alarmas */
+puuu = function(plg,co) {
+    switch (plg > 0 && co > 0) {
+        case (plg > 1000 && co > 100):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Peligro');
+            $('#message-alert').html('Los niveles de PLG y CO se encuentran en estado crítico.');
+        break;
+
+        case (plg > 1000 && co > 70):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Peligro');
+            $('#message-alert').html('El nivel de PLG se encuentra en estado crítico y el de CO se encuentra elevado');
+        break;
+
+        case (plg > 700 && co > 100):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Peligro');
+            $('#message-alert').html('El nivel de PLG se encuentra elevado y el de CO se encuentra en estado crítico');
+        break;
+
+        case (plg > 1000):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Peligro');
+            $('#message-alert').html('El nivel de PLG se encuentra en estado crítico.');
+        break;
+
+        case (co > 100):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Peligro');
+            $('#message-alert').html('El nivel de CO se encuentra en estado crítico.');
+        break;
+
+        case (plg > 700 && co > 70):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Alerta');
+            $('#message-alert').html('Los niveles de PLG y CO se encuentran muy elevados.');
+        break;
+
+        case (plg > 700):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Alerta');
+            $('#message-alert').html('El nivel de PLG se encuentra muy elevado.');
+        break;
+
+        case (co > 70):
+            $('#card-alert').removeClass('invisible');
+            $('#card-title-alert').html(' Alerta');
+            $('#message-alert').html('El nivel de CO se encuentra muy elevado.');   
+        break;
+
+        default:
+            if ($('#card-alert').classListContains('invisible')) {
+
+            }else {
+                $('#card-alert').addClass('invisible'); 
+            }                   
+    }
 };
